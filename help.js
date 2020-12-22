fb327f59d466b5811c15b4275324c1f40004f3dc
A187ID <73683578+A187ID@users.noreply.github.com>
2020-12-22 12:57:19 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,74 @@
+
+const help = (prefix) => { 
+	return `                 
+┏━━━°❀ ❬ 𝘼𝘽𝙊𝙐𝙏 ❭ ❀°━━━┓
+┃
+┏❉ *${prefix}owner*
+┣❉ *${prefix}donasi*
+┗❉ *${prefix}info*
+┃
+┣━━━°❀ ❬ 𝗠𝗔𝗞𝗘𝗥 ❭ ❀°━━━⊱
+┃
+┣➥ *${prefix}sticker*
+┣➥ *${prefix}tsticker*
+┣➥ *${prefix}nulis*
+┃
+┣━━━━°❀ ❬ 𝙈𝙀𝘿𝙄𝘼 ❭ ❀°━━━⊱
+┃
+┣➥ *${prefix}tts*
+┣➥ *${prefix}tiktok*
+┣➥ *${prefix}meme*
+┣➥ *${prefix}memeindo*
+┣➥ *${prefix}nsfwloli*
+┣➥ *${prefix}ocr*
+┣➥ *${prefix}loli*
+┃
+┣━━━°❀ ❬ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 ❭ ❀°━━⊱
+┃
+┣➥ *yt* [link]
+┣➥ *play* [judul lagu]
+┃
+┣━━━━°❀ ❬ 𝙂𝙍𝙊𝙐𝙋 ❭ ❀°━━━━⊱
+┃
+┣➥ *${prefix}add* [62xxx]
+┣➥ *${prefix}kick* [tag]
+┣➥ *${prefix}setpp*
+┣➥ *${prefix}demote* [tag]
+┣➥ *${prefix}promote* [tag]
+┣➥ *${prefix}setpp*
+┣➥ *${prefix}group* [buka/tutup]
+┣➥ *${prefix}welcome* [1/0]
+┣➥ *${prefix}nsfw* [1/0]
+┣➥ *${prefix}simih* [1/0]
+┃
+┣━━━━━°❀ ❬ 𝙊𝙒𝙉𝙀𝙍 ❭ ❀°━━━━⊱
+┃
+┣➥ *${prefix}bc* 
+┣➥ *${prefix}leave*
+┣➥ *${prefix}clearall*
+┣➥ *${prefix}setprefix*
+┣➥ *${prefix}clone* [tag]
+┣➥ *${prefix}block*
+┣➥ *${prefix}unblock*
+┣➥ *${prefix}getses*
+┃
+┣━━━━°❀ ❬ 𝙊𝙏𝙃𝙀𝙍 ❭ ❀°━━━━━⊱
+┃
+┣➥ *${prefix}ytsearch*
+┣➥ *${prefix}listadmin*
+┣➥ *${prefix}blocklist*
+┣➥ *${prefix}simi*
+┣➥ *${prefix}wait*
+┣➥ *${prefix}fitnah*
+┣➥ *${prefix}tiktokstalk*
+┣➥ *${prefix}url2img*
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃ 
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+exports.help = help
+
+
+
+  
